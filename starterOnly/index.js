da240ef344132daf0015bdb14662402f248e989d
Rudy <Rudy.26.fr@gmail.com>
2022-12-12 16:27:26 +0100

    conditions and display error

--- starterOnly/index.js
+++ starterOnly/index.js
@@ -5,6 +5,7 @@ import modalFactory from './factory/modal.js';
 const modalBtn = document.querySelectorAll(".modal-btn"); // querySelector pour bouton mobile et desktop
 const closeBtn = document.getElementById("close");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById('form')
 
 
 var modal = modalFactory()
@@ -18,6 +19,88 @@ closeBtn.addEventListener("click", () => modal.closeModal());
 
 
 
+form.addEventListener('submit', function (e) {
+
+    var first = document.getElementById('first').value;
+    var last = document.getElementById('last').value;
+    var email = document.getElementById('email').value;
+    var birthdate = document.getElementById('birthdate').value;
+    var quantity = Number(document.getElementById('quantity').value);
+    var locations = document.getElementsByName('location');
+    var checkbox1 = document.getElementById('checkbox1').checked;
+    var checkbox2 = document.getElementById('checkbox2').value;
+
+
+    // Test recommandations champs du formulaire
+    if (checkMinLength(first, 2))
+        if (checkMinLength(last, 2))
+            if (mailIsValid(email))
+                if (checkMinLength(birthdate, 10))
+                    if (checkMinLength(String(quantity), 1))
+                        if (checkNodeIsChecked(locations))
+                            if (checkbox1 === true) {
+                                modal.closeModal()
+                                document.getElementById('form-succes').style.display = "block"
+                                setTimeout(() => {
+                                    document.getElementById('form-succes').style.display = "none"
+                                }, 4000)
+                            }
+    // return true; // Si formulaire bon, fin execution
+
+
+    if (!checkMinLength(first, 2))
+        writeError('error-first', "Veuillez entrer 2 caractères ou plus pour le champ du prénom.")
+    if (!checkMinLength(last, 2))
+        writeError('error-last', "Veuillez entrer 2 caractères ou plus pour le champ du nom.")
+    if (!mailIsValid(email))
+        writeError('error-email', "Veuillez entrer un email valide.")
+    if (!checkMinLength(birthdate, 10))
+        writeError('error-birthdate', "Veuillez entrer un une date de naissance.")
+    if (!checkMinLength(String(quantity), 1))
+        writeError('error-quantity', "Veuillez entrer le nombre de tournois auquel vous avez participé")
+    if (!checkNodeIsChecked(locations))
+        writeError('error-location', "Veuillez renseigner un tournois")
+    if (checkbox1 === false)
+        writeError('error-conditions', "Veuillez renseigner un tournois")
+
+
+    // Si les champs ne suivent pas les recommandations, le questionnaire n'est pas envoyé
+    e.preventDefault();
+
+})
+
+
+// -----
+// FONCTIONS POUR VERIFIER LE FORMULAIRE
+// -----
+
+
+function mailIsValid(mail) {
+    var regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return regexEmail.test(mail);
+}
+
+function checkMinLength(string, length) {
+    if (string.length >= length)
+        return true;
+    return false;
+}
+
+function checkNodeIsChecked(nodes) {
+    for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].checked === true) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+function writeError(id, message) {
+    document.getElementById(id).innerText = message
+}
+
+
 
 
 
@@ -28,77 +111,5 @@ function editNav() {
     } else {
         x.className = "topnav";
     }
-}
-
-
-
-
-
-
-// form
-
-// Function appelée après la validation du formulaire
-function validate() {
-    var first = document.getElementById('first').value;
-    var last = document.getElementById('last').value;
-    var email = document.getElementById('email').value;
-    var birthdate = document.getElementById('birthdate').value;
-    var quantity = document.getElementById('quantity').value;
-    quantity = Number(quantity);
-    var locations = document.getElementsByName('location');
-    var checkbox1 = document.getElementById('checkbox1').checked;
-    var checkbox2 = document.getElementById('checkbox2').value;
-
-    var regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-
-    var locactionCheck = false
-    var locationName = false
-    locations.forEach((e) => {
-        if (e.checked) {
-            locactionCheck = true;
-            locationName = e.value;
-        }
-    })
-
-
-    // Si prenom a 2 caractères ou plus
-    if (first.length >= 2) {
-        // Si nom a 2 caractères ou plus
-        if (last.length >= 2) {
-            // Si Le format de l'email est valide
-            if (regexEmail.test(email)) {
-                // Si la quantité est bien une valeur numérique
-                if (typeof quantity == "number") {
-                    // Si une location a été cochée
-                    if (locactionCheck === true) {
-                        // Si les conditions générales sont cochés
-                        if (checkbox1 === true) {
-                            // Toute les validations sont passés
-                            return true;
-                        }
-                        else {
-                            return false
-                        }
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                else {
-                    return false;
-                }
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            return false;
-        }
-    }
-    else {
-        return false;
-    }
-
 }
 
